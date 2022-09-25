9c56a3d9f39db6632a43446183101f8ccbe1cb01
Ed Beroset <beroset@ieee.org>
2022-09-25 18:59:49 -0400

    Add WeatherParser class
    This is the initial check in of code to implement the weather data
    parsing.
    
    Signed-off-by: Ed Beroset <beroset@ieee.org>

---
+++ src/weatherparser.cpp
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2022 - Ed Beroset <beroset@ieee.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <QDebug>
+#include "weatherparser.h"
+
+WeatherParser::WeatherParser(QObject *parent) :
+    QObject(parent)
+{
+}
+
+
+void WeatherParser::updateWeather(QString weatherJson)
+{
+    qWarning("Json weather string: '%s'",
+                 weatherJson.toStdString().c_str());
+}
