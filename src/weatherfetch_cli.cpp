66dbf62985c5b56e37260187c18a785ab33b6bba
Ed Beroset <beroset@ieee.org>
2022-10-07 19:04:12 -0400

    Add a commmand line weatherfetch_cli
    This fetches the weather data based on the current first entry in the
    asteroid-weatherfetch configuration and then populates the settings that
    are used by asteroid-weater.

---
+++ src/weatherfetch_cli.cpp
@@ -0,0 +1,28 @@
+
+#include "WeatherFetcher.h"
+#include "WeatherParser.h"
+#include "UrlFetcher.h" 
+
+#include <QCoreApplication>
+#include <QUrl>
+#include <functional>
+
+int main(int argc, char *argv[])
+{
+    using namespace std::placeholders;
+    QCoreApplication app(argc, argv);
+    QCoreApplication::setApplicationName("weatherfetch_cli");
+    QCoreApplication::setApplicationVersion("1.0");
+    WeatherFetcher wf;
+    UrlFetcher fetcher;
+    WeatherParser parser;
+    QObject::connect(&fetcher, &UrlFetcher::receivedData, &wf, &WeatherFetcher::receivedData, Qt::DirectConnection);
+    QObject::connect(&wf, &WeatherFetcher::update, &parser, &WeatherParser::update, Qt::DirectConnection);
+    QObject::connect(&parser, &WeatherParser::done, &app, &QCoreApplication::quit, Qt::DirectConnection);
+    auto url{wf.createUrl()};
+    qInfo() << "Setting url to " << url;
+    fetcher.fetch(url);
+    return app.exec();
+}
+
+
