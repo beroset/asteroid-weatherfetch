8662435e1ae6fe16ab799fea4862b616ca6cdcba
Ed Beroset <beroset@ieee.org>
2022-10-09 09:40:16 -0400

    Add German translation

---
+++ i18n/asteroid-weatherfetch.de_DE.ts
@@ -0,0 +1,70 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name></name>
+    <message id="id-unknown">
+        <location filename="../src/main.qml" line="40"/>
+        <source>Unknown</source>
+        <translation>unbekannte</translation>
+    </message>
+    <message id="id-weatherfetch">
+        <location filename="../src/main.qml" line="141"/>
+        <source>Fetch weather data</source>
+        <translation>Wetterdaten abrufen</translation>
+    </message>
+    <message id="id-weatherfetch-addlocation">
+        <location filename="../src/main.qml" line="172"/>
+        <source>Add location</source>
+        <translation>Ort hinzufügen</translation>
+    </message>
+    <message id="id-weatherfetch-dellocation">
+        <location filename="../src/main.qml" line="205"/>
+        <source>Delete last location</source>
+        <translation>letzten Standort löschen</translation>
+    </message>
+    <message id="id-weatherfetch-unchanged">
+        <location filename="../src/main.qml" line="232"/>
+        <source>Weather data unchanged</source>
+        <translation>Wetterdaten unverändert</translation>
+    </message>
+    <message id="id-weatherfetch-success">
+        <location filename="../src/main.qml" line="234"/>
+        <source>Weather fetch succeeded</source>
+        <translation>Wetterdaten aktualisiert</translation>
+    </message>
+    <message id="id-weatherfetch-fail">
+        <location filename="../src/main.qml" line="236"/>
+        <source>Weather fetch failed</source>
+        <translation>Wetterdatenabruf fehlgeschlagen</translation>
+    </message>
+    <message id="id-weatherfetch-fetch">
+        <location filename="../src/main.qml" line="252"/>
+        <source>Fetch weather data</source>
+        <translation>Wetterdaten abrufen</translation>
+    </message>
+    <message id="id-app-launcher-name">
+        <location filename="asteroid-weatherfetch.desktop.h" line="6"/>
+        <source>Weather fetch app</source>
+        <translation>Wetterdaten Anwendung</translation>
+    </message>
+</context>
+<context>
+    <name>LocationPicker</name>
+    <message>
+        <location filename="../src/LocationPicker.qml" line="52"/>
+        <source>Select location</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/LocationPicker.qml" line="60"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/LocationPicker.qml" line="76"/>
+        <source>Name:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
