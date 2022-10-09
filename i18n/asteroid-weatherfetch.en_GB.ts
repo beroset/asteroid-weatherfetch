949766f3edd41656faa37823fdbf020b83bbffda
Ed Beroset <beroset@ieee.org>
2022-10-09 10:17:49 -0400

    Add English language translation file

---
+++ i18n/asteroid-weatherfetch.en_GB.ts
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name></name>
+    <message id="id-unknown">
+        <location filename="../src/main.qml" line="40"/>
+        <source>Unknown</source>
+        <translation>unknown</translation>
+    </message>
+    <message id="id-weatherfetch">
+        <location filename="../src/main.qml" line="141"/>
+        <source>Fetch weather data</source>
+        <translation>Fetch weather data</translation>
+    </message>
+    <message id="id-weatherfetch-addlocation">
+        <location filename="../src/main.qml" line="172"/>
+        <source>Add location</source>
+        <translation>Add location</translation>
+    </message>
+    <message id="id-weatherfetch-dellocation">
+        <location filename="../src/main.qml" line="205"/>
+        <source>Delete last location</source>
+        <translation>Delete last location</translation>
+    </message>
+    <message id="id-weatherfetch-unchanged">
+        <location filename="../src/main.qml" line="232"/>
+        <source>Weather data unchanged</source>
+        <translation>Weather data unchanged</translation>
+    </message>
+    <message id="id-weatherfetch-success">
+        <location filename="../src/main.qml" line="234"/>
+        <source>Weather fetch succeeded</source>
+        <translation>Weather fetch succeeded</translation>
+    </message>
+    <message id="id-weatherfetch-fail">
+        <location filename="../src/main.qml" line="236"/>
+        <source>Weather fetch failed</source>
+        <translation>Weather fetch failed</translation>
+    </message>
+    <message id="id-weatherfetch-fetch">
+        <location filename="../src/main.qml" line="252"/>
+        <source>Fetch weather data</source>
+        <translation>Fetch weather data</translation>
+    </message>
+</context>
+</TS>
