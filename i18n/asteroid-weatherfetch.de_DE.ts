5e995408a02092dc8616d8d3161c9fc0a788b7c7
Ed Beroset <beroset@ieee.org>
2022-12-26 10:08:58 -0500

    Update translation files
    Signed-off-by: Ed Beroset <beroset@ieee.org>

--- i18n/asteroid-weatherfetch.de_DE.ts
+++ i18n/asteroid-weatherfetch.de_DE.ts
@@ -9,62 +9,59 @@
         <translation>unbekannte</translation>
     </message>
     <message id="id-weatherfetch">
-        <location filename="../src/main.qml" line="141"/>
+        <location filename="../src/main.qml" line="151"/>
         <source>Fetch weather data</source>
         <translation>Wetterdaten abrufen</translation>
     </message>
     <message id="id-weatherfetch-addlocation">
-        <location filename="../src/main.qml" line="172"/>
+        <location filename="../src/main.qml" line="182"/>
         <source>Add location</source>
         <translation>Ort hinzufügen</translation>
     </message>
     <message id="id-weatherfetch-dellocation">
-        <location filename="../src/main.qml" line="205"/>
+        <location filename="../src/main.qml" line="210"/>
         <source>Delete last location</source>
         <translation>letzten Standort löschen</translation>
     </message>
+    <message id="id-weatherfetch-trying">
+        <location filename="../src/main.qml" line="237"/>
+        <source>Attempting to fetch weather data</source>
+        <translation type="unfinished"></translation>
+    </message>
     <message id="id-weatherfetch-unchanged">
-        <location filename="../src/main.qml" line="232"/>
+        <location filename="../src/main.qml" line="243"/>
         <source>Weather data unchanged</source>
         <translation>Wetterdaten unverändert</translation>
     </message>
     <message id="id-weatherfetch-success">
-        <location filename="../src/main.qml" line="234"/>
+        <location filename="../src/main.qml" line="245"/>
         <source>Weather fetch succeeded</source>
         <translation>Wetterdaten aktualisiert</translation>
     </message>
     <message id="id-weatherfetch-fail">
-        <location filename="../src/main.qml" line="236"/>
+        <location filename="../src/main.qml" line="247"/>
         <source>Weather fetch failed</source>
         <translation>Wetterdatenabruf fehlgeschlagen</translation>
     </message>
     <message id="id-weatherfetch-fetch">
-        <location filename="../src/main.qml" line="252"/>
+        <location filename="../src/main.qml" line="265"/>
         <source>Fetch weather data</source>
         <translation>Wetterdaten abrufen</translation>
     </message>
-    <message id="id-app-launcher-name">
-        <location filename="asteroid-weatherfetch.desktop.h" line="6"/>
-        <source>Weather fetch app</source>
-        <translation>Wetterdaten Anwendung</translation>
+    <message id="id-location-name-field">
+        <location filename="../src/LocationDialog.qml" line="50"/>
+        <source>Location Name</source>
+        <translation>Standortname</translation>
     </message>
-</context>
-<context>
-    <name>LocationPicker</name>
-    <message>
-        <location filename="../src/LocationPicker.qml" line="52"/>
-        <source>Select location</source>
-        <translation type="unfinished"></translation>
+    <message id="id-latitude-field">
+        <location filename="../src/LocationDialog.qml" line="58"/>
+        <source>Location Latitude</source>
+        <translation>Breitengrad des Standorts</translation>
     </message>
-    <message>
-        <location filename="../src/LocationPicker.qml" line="60"/>
-        <source>Cancel</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/LocationPicker.qml" line="76"/>
-        <source>Name:</source>
-        <translation type="unfinished"></translation>
+    <message id="id-longitude-field">
+        <location filename="../src/LocationDialog.qml" line="67"/>
+        <source>Location Longitude</source>
+        <translation>Längengrad des Standorts</translation>
     </message>
 </context>
 </TS>
--- i18n/asteroid-weatherfetch.en_GB.ts
+++ i18n/asteroid-weatherfetch.en_GB.ts
@@ -9,39 +9,59 @@
         <translation>unknown</translation>
     </message>
     <message id="id-weatherfetch">
-        <location filename="../src/main.qml" line="141"/>
+        <location filename="../src/main.qml" line="151"/>
         <source>Fetch weather data</source>
         <translation>Fetch weather data</translation>
     </message>
     <message id="id-weatherfetch-addlocation">
-        <location filename="../src/main.qml" line="172"/>
+        <location filename="../src/main.qml" line="182"/>
         <source>Add location</source>
         <translation>Add location</translation>
     </message>
     <message id="id-weatherfetch-dellocation">
-        <location filename="../src/main.qml" line="205"/>
+        <location filename="../src/main.qml" line="210"/>
         <source>Delete last location</source>
         <translation>Delete last location</translation>
     </message>
+    <message id="id-weatherfetch-trying">
+        <location filename="../src/main.qml" line="237"/>
+        <source>Attempting to fetch weather data</source>
+        <translation type="unfinished"></translation>
+    </message>
     <message id="id-weatherfetch-unchanged">
-        <location filename="../src/main.qml" line="232"/>
+        <location filename="../src/main.qml" line="243"/>
         <source>Weather data unchanged</source>
         <translation>Weather data unchanged</translation>
     </message>
     <message id="id-weatherfetch-success">
-        <location filename="../src/main.qml" line="234"/>
+        <location filename="../src/main.qml" line="245"/>
         <source>Weather fetch succeeded</source>
         <translation>Weather fetch succeeded</translation>
     </message>
     <message id="id-weatherfetch-fail">
-        <location filename="../src/main.qml" line="236"/>
+        <location filename="../src/main.qml" line="247"/>
         <source>Weather fetch failed</source>
         <translation>Weather fetch failed</translation>
     </message>
     <message id="id-weatherfetch-fetch">
-        <location filename="../src/main.qml" line="252"/>
+        <location filename="../src/main.qml" line="265"/>
         <source>Fetch weather data</source>
         <translation>Fetch weather data</translation>
+    </message>
+    <message id="id-location-name-field">
+        <location filename="../src/LocationDialog.qml" line="50"/>
+        <source>Location Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-latitude-field">
+        <location filename="../src/LocationDialog.qml" line="58"/>
+        <source>Location Latitude</source>
+        <translation>Location latitude</translation>
+    </message>
+    <message id="id-longitude-field">
+        <location filename="../src/LocationDialog.qml" line="67"/>
+        <source>Location Longitude</source>
+        <translation>Location longitude</translation>
     </message>
 </context>
 </TS>
