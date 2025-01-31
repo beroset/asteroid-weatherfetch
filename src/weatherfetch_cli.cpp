228020b8d0b21d9de189fd5bdd36e0f71c7f2c5b
Ed Beroset <beroset@ieee.org>
2025-01-31 14:09:33 -0500

    Implement new cli options
    This adds the add, config, delete, rotate and list command-line options
    and also implements help and version.
    
    Signed-off-by: Ed Beroset <beroset@ieee.org>

--- src/weatherfetch_cli.cpp
+++ src/weatherfetch_cli.cpp
@@ -4,6 +4,7 @@
 #include "UrlFetcher.h"
 
 #include <QCoreApplication>
+#include <QCommandLineParser>
 #include <QUrl>
 #include <cstdlib>
 #include <functional>
@@ -13,23 +14,44 @@ int main(int argc, char *argv[])
     using namespace std::placeholders;
     QCoreApplication app(argc, argv);
     QCoreApplication::setApplicationName("weatherfetch_cli");
-    QCoreApplication::setApplicationVersion("1.0");
+    QCoreApplication::setApplicationVersion("2.0");
+    QCommandLineParser opts;
+    opts.addHelpOption();
+    opts.addVersionOption();
+    opts.addOptions({
+        {{"a", "add"},
+            QCoreApplication::translate("main", "add location to top of list (TOL)."),
+            QCoreApplication::translate("main", "lat,lng,cityname")
+        },
+        {{"c", "config"},
+            QCoreApplication::translate("main", "manipulate the list but don't fetch weather.")},
+        {{"d", "delete"},
+            QCoreApplication::translate("main", "delete bottom of list (BOL).")},
+        {{"l", "list"},
+            QCoreApplication::translate("main", "list all current locations.")},
+        {{"r", "rotate"},
+            QCoreApplication::translate("main", "rotate the list to put TOL at BOL.")},
+    });
     WeatherSettings ws;
-    if (argc >= 4) {
-        // looks like we may have been passed a new location
-        int i{0};
-        float lat{static_cast<float>(std::atof(argv[++i]))};
-        float lng{static_cast<float>(std::atof(argv[++i]))};
-        QString cityname{};
-        for (++i; i < argc; ++i) {
-            cityname += argv[i];
-            if (i != argc-1) {
-                cityname += " ";
-            }
-        }
-        ws.addLocation(lat, lng, cityname);
-        ws.update();
+    opts.process(app);
+    if (opts.isSet("rotate")) {
+        ws.rotate();
     }
+    if (opts.isSet("delete")) {
+        ws.removeLast();
+    }
+    if (opts.isSet("add")) {
+        QString location = opts.value("add");
+        QString lat = location.section(u',',0,0);
+        QString lng = location.section(u',',1,1);
+        QString cityname = location.section(u',',2);
+        ws.addLocation(lat.toFloat(), lng.toFloat(), cityname);
+    }
+    if (opts.isSet("list")) {
+        std::cout << ws << "\n";
+    }
+    bool fetch = !opts.isSet("config");
+    ws.update();
     UrlFetcher fetcher;
     WeatherParser parser;
     QObject::connect(&fetcher,
@@ -40,7 +62,12 @@ int main(int argc, char *argv[])
     QObject::connect(&parser, &WeatherParser::done, &app, &QCoreApplication::quit, Qt::DirectConnection);
     auto url{parser.createUrl(ws.getCityName(), ws.getCityLatitude(), ws.getCityLongitude(), ws.getApikey())};
     qInfo() << "Setting url to " << url;
-    fetcher.fetch(url);
+    if (fetch) {
+        fetcher.fetch(url);
+    } else {
+        // immediate quit if we're in config mode
+        QTimer::singleShot(0, &app, &QCoreApplication::quit);
+    }
     return app.exec();
 }
 
